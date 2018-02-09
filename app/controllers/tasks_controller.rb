0ae15f849a8c73caab84e46860220e9ab3169909
EC2 Default User <ec2-user@ip-172-31-64-66.ec2.internal>
2018-02-09 13:58:55 +0000

    retry2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,7 @@ class TasksController < ApplicationController
   end
   
   def show
-    @task = Task.find(paramas[:id])
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -49,8 +49,8 @@ class TasksController < ApplicationController
   private
 
   # Strong Parameter
-  def message_params
-    params.require(:message).permit(:content)
+  def task_params
+    params.require(:task).permit(:content)
   end
   
   
