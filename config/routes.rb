ae32597eef0b5d2b192ab6f77617bf64c2feba18
EC2 Default User <ec2-user@ip-172-31-64-66.ec2.internal>
2018-02-18 12:50:25 +0000

    tasklist login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
   root to: 'tasks#index'
+  
+   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  
+  get 'signup' , to: 'users#new'
   resources :tasks
+  resources :users,only:[:new,:create]
 end
